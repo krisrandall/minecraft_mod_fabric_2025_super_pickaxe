089865d88af4863f299362a8a5c49adbee2c4686
Nicholas Randall <nick@jdoodle.com>
2025-08-27 11:55:51 +0200

    made OP Pickaxe enchantable

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,13 +1,17 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.EnchantingContext;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,6 +35,10 @@ public class ExampleMod implements ModInitializer {
         @Override
         public boolean isCorrectForDrops(net.minecraft.item.ItemStack stack, net.minecraft.block.BlockState state) {
             return true; // Can harvest ALL blocks
+        }
+        @Override
+        public boolean canBeEnchantedWith(ItemStack stack, RegistryEntry<Enchantment> enchantment, EnchantingContext context) {
+            return true; // Can be enchanted
         }
 
     }
